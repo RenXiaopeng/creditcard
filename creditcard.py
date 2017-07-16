645e02a93e893d88305b44f32f562ff6f31ff103
renxiaopeng <531920754@sina.com>
2017-07-16 18:36:03 +0800

    credit card class

---
+++ creditcard.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class CreditCard:
+
+    def __int__(self, number, bank, card_property, last_four_number, validity, three_number, holder_name, accountant_bill_date, due_date, fixed_account, available_account):
+        self.number = number;
+        self.bank = bank;
+        self.card_property = card_property;
+        self.last_four_number = last_four_number;
